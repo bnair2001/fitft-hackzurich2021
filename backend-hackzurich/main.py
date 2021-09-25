da35b092c88b142f098624998394212d6e8787b4
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2021-09-25 16:19:54 +0930

    Enabled CORS

--- backend-hackzurich/main.py
+++ backend-hackzurich/main.py
@@ -3,8 +3,16 @@ from models import *
 from users import *
 from connection import *
 from fitnessFunctions import *
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
